5776f6d12b341f358ba6eeffdbb0ea313305bfb8
elijahsnoz <elijahsnoz@gmail.com>
2024-04-30 02:06:22 +0100

    insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,32 +9,32 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *insertion_point;
+    listint_t *current, *insertion_point;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	current = (*list)->next;
+    current = (*list)->next;
 
-	while (current != NULL)
-	{
-		insertion_point = current->prev;
-		while (insertion_point != NULL && insertion_point->n > current->n)
-		{
-			if (insertion_point->prev != NULL)
-				insertion_point->prev->next = current;
-			current->prev = insertion_point->prev;
-			insertion_point->prev = current;
-			insertion_point->next = current->next;
-			if (current->next != NULL)
-				current->next->prev = insertion_point;
-			current->next = insertion_point;
-			if (*list == insertion_point)
-				*list = current;
+    while (current != NULL)
+    {
+        insertion_point = current->prev;
+        while (insertion_point != NULL && insertion_point->n > current->n)
+        {
+            if (insertion_point->prev != NULL)
+                insertion_point->prev->next = current;
+            current->prev = insertion_point->prev;
+            insertion_point->prev = current;
+            insertion_point->next = current->next;
+            if (current->next != NULL)
+                current->next->prev = insertion_point;
+            current->next = insertion_point;
+            if (*list == insertion_point)
+                *list = current;
 
-			print_list(*list);
-			insertion_point = current->prev;
-		}
-		current = current->next;
-	}
+            print_list(*list);
+            insertion_point = current->prev;
+        }
+        current = current->next;
+    }
 }
