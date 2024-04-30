6542ba07e599f92a1bf79b131a3bf83db5f86a1f
elijahsnoz <elijahsnoz@gmail.com>
2024-04-30 22:13:50 +0100

    insertion_sort_list

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,95 +1,74 @@
 #include "sort.h"
-
-void swap_ints(int *a, int *b);
-int lomuto_partition(int *array, size_t size, int left, int right);
-void lomuto_sort(int *array, size_t size, int left, int right);
-void quick_sort(int *array, size_t size);
+#include <stdio.h>
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * lomuto_partition - Order a subset of an array of integers according to
- *                    the lomuto partition scheme (last element as pivot).
- * @array: The array of integers.
+ * quick_sort - Sorts an array of integers in ascending order
+ *              using the Quick sort algorithm.
+ * @array: The array to sort.
  * @size: The size of the array.
- * @left: The starting index of the subset to order.
- * @right: The ending index of the subset to order.
- *
- * Return: The final partition index.
- */
-int lomuto_partition(int *array, size_t size, int left, int right)
-{
-	int *pivot, above, below;
-
-	pivot = array + right;
-	for (above = below = left; below < right; below++)
-	{
-		if (array[below] < *pivot)
-		{
-			if (above < below)
-			{
-				swap_ints(array + below, array + above);
-				print_array(array, size);
-			}
-			above++;
-		}
-	}
-
-	if (array[above] > *pivot)
-	{
-		swap_ints(array + above, pivot);
-		print_array(array, size);
-	}
-
-	return (above);
-}
-
-/**
- * lomuto_sort - Implement the quicksort algorithm through recursion.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- * @left: The starting index of the array partition to order.
- * @right: The ending index of the array partition to order.
- *
- * Description: Uses the Lomuto partition scheme.
- */
-void lomuto_sort(int *array, size_t size, int left, int right)
-{
-	int part;
-
-	if (right - left > 0)
-	{
-		part = lomuto_partition(array, size, left, right);
-		lomuto_sort(array, size, left, part - 1);
-		lomuto_sort(array, size, part + 1, right);
-	}
-}
-
-/**
- * quick_sort - Sort an array of integers in ascending
- *              order using the quicksort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
- *
- * Description: Uses the Lomuto partition scheme. Prints
- *              the array after each swap of two elements.
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	lomuto_sort(array, size, 0, size - 1);
+    _quick_sort(array, 0, size - 1, size);
+}
+
+/**
+ * _quick_sort - Recursive function to perform quick sort.
+ * @array: The array to sort.
+ * @low: The low index of the partition to sort.
+ * @high: The high index of the partition to sort.
+ * @size: The size of the array.
+ */
+void _quick_sort(int *array, int low, int high, size_t size)
+{
+    if (low < high)
+    {
+        int pi = _partition(array, low, high, size);
+
+        _quick_sort(array, low, pi - 1, size);
+        _quick_sort(array, pi + 1, high, size);
+    }
+}
+
+/**
+ * _partition - Partitions the array and returns the pivot index.
+ * @array: The array to partition.
+ * @low: The low index of the partition.
+ * @high: The high index of the partition.
+ * @size: The size of the array.
+ *
+ * Return: The index of the pivot element.
+ */
+int _partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (array[j] < pivot)
+        {
+            i++;
+            _swap(&array[i], &array[j]);
+            print_array(array, size);
+        }
+    }
+    _swap(&array[i + 1], &array[high]);
+    print_array(array, size);
+    return (i + 1);
+}
+
+/**
+ * _swap - Swaps two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ */
+void _swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
